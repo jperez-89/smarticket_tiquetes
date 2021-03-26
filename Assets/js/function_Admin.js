15ea9f0021361a8db9126b06e94af9129f8402e5
jperez-89 <jrwc1989@gmail.com>
2021-03-26 12:17:11 -0600

    Actualizando cards

--- Assets/js/function_Admin.js
+++ Assets/js/function_Admin.js
@@ -5,24 +5,38 @@ document.addEventListener("DOMContentLoaded", function () {
 window.addEventListener(
   "load",
   function () {
-    obtenerCantProductos();
+    getCantProducts();
+    getCantClients()
   },
   false
 );
 
-function obtenerCantProductos() {
+function getCantProducts() {
   // Obtener los datos
-  var request = window.XMLHttpRequest
-    ? new XMLHttpRequest()
-    : new ActiveXObject("Microsoft.XMLHTTP");
-  var UrlGetProductos = base_url + "Productos/getCantProductos";
-  request.open("GET", UrlGetProductos, true);
+  var request = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+  var url = base_url + "Productos/getCantProductos";
+  request.open("GET", url, true);
   request.send();
 
   request.onreadystatechange = function () {
     if (request.readyState == 4 && request.status == 200) {
       var objData = JSON.parse(request.responseText);
       document.querySelector("#cantProductos").innerHTML = objData[0].Cantidad;
+    }
+  };
+}
+
+function getCantClients() {
+  // Obtener los datos
+  var request = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+  var url = base_url + "Clientes/getCantClients";
+  request.open("GET", url);
+  request.send();
+
+  request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+      var objData = JSON.parse(request.responseText);
+      document.querySelector("#cantClientes").innerHTML = objData[0].Cantidad;
     }
   };
 }
